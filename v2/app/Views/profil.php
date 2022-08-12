e4efc8528cf9c8acec2a2ea9cfd519d476dac0ba
pramudeka <goughparliecke@gmail.com>
2022-08-12 15:30:55 +0700

    again

--- v2/app/Views/profil.php
+++ v2/app/Views/profil.php
@@ -6,12 +6,12 @@
 <section class="page-section  flex-grow-1" id="profil-body">
     <div class="container">
         <div class="row profil-title">
-            <div id="prof1" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="500">
+            <div id="prof1" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="0">
                 <h5 >Visi</h5>
                 <p id="visi">Terwujudnya Kabupaten Blitar Yang Mandiri dan Sejahtera Berlandaskan Akhlak Mulia. Baldatun, Toyyibatun, Warobbun Ghofur</p>
             </div>
 
-            <div id="prof2" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="700" data-aos-anchor="#prof1">
+            <div id="prof2" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="250" data-aos-anchor="#prof1">
                 <h5 >Misi</h5>
                 <ul id="misi">
                     <li>Meningkatkan kesejahteraan sosial masyarakat Blitar berlandaskan iman dan takwa dengan kearifan lokal budaya</li>
@@ -20,14 +20,14 @@
                     <li>Percepatan dan pemerataan pembangunan yang adil dan merata melalui pengembangan potensi ekonomi daerah dengan mengedepankan pemberdayaan masyarakat dan kelestarian lingkungan</li>
                 </ul>
             </div>
-            <div id="prof3" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="900" data-aos-anchor="#prof2">
+            <div id="prof3" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="500" data-aos-anchor="#prof2">
                 <h5 >Luas dan Batas Wilayah</h5>
                 <p>Sebelah Utara berbatasan dengan Kabupaten Kediri Sebelah Timur berbatasan dengan Kabupaten Malang Sebelah Selatan berbatasan dengan Samudra Indonesia Sebelah Barat berbatasan dengan Kabupaten Tulungagung.</p>
 
                 <p>Luas wilayah Kabupaten Blitar adalah 1.588,79 km2 dimana sekitar 38,02 persen merupakan wilayah dataran tinggi yang berada pada ketinggian 300-420 dari permukaan laut. Oleh karena di sebelah selatan berbatasan langsung dengan Samudra Indonesia, maka beberapa kecamatan mempunyai wilayah pantai, yaitu Kecamatan Panggungrejo, Wonotirto, dan Wates.</p>
             </div>
 
-            <div id="prof4" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="1200" data-aos-anchor="#prof3">
+            <div id="prof4" class="profil-item mb-4" data-aos="fade-right" data-aos-duration="500" data-aos-delay="750" data-aos-anchor="#prof3">
                 <h5 >Letak dan Kondisi Geografis</h5>
                 <p>Secara astronomis Kabupaten Blitar terletak di 111°40’ – 112°10’ BT dan 7°58’ – 8°9’51’’ LS, sedangkan secara geografi s terletak di Pulau Jawa bagian timur dan berada di pesisir Samudra Hindia. Secara administratif Kabupaten Blitar terbagi dalam 22 kecamatan, terdiri dari 248 desa/kelurahan yaitu, 28 kelurahan dan 220 desa. Pemekaran wilayah kecamatan ini dimulai pada tahun 1992, sedangkan sebelum tahun tersebut Kabupaten Blitar hanya terdiri atas 19 kecamatan.</p>
 
